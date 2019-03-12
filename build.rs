011cea5b9ad5048e75a352146fb8ef4cbe9fa177
Jannik Lorenz <dev@janniklorenz.de>
2019-03-12 22:26:13 +0100

    add -std=c18

--- build.rs
+++ build.rs
@@ -3,6 +3,6 @@ extern crate cc;
 fn main() {
     cc::Build::new()
         .file("src/device_api/esa/esa.c")
-        .flag_if_supported("-std=c99")
+        .flag_if_supported("-std=c18")
         .compile("esa_c");
 }
