b699045047761a3066aced6601a4fadec4a7f375
Jannik Lorenz <dev@janniklorenz.de>
2019-03-12 22:48:00 +0100

    test c11

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ extern crate cc;
 fn main() {
     cc::Build::new()
         .file("src/device_api/esa/esa.c")
-        .flag_if_supported("-std=gnu99")
+        // .flag_if_supported("-std=gnu99")
+        .flag_if_supported("-std=c11")
         .compile("esa_c");
 }
